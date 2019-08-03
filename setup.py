133ac928e4165e71ceaa458a5f80dd52233e93a2
Eden <zhangchi@yunify.com>
2019-08-03 22:28:26 +0800

    fix

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='disktool',
+    version='1.0.0',
+    description='Disk tool that integrates smartool and megacli information',
+    keywords='qingstor cli smartool megacli',
+    author='Yunify Qingstor Team',
+    author_email='zhangchi@yunify.com',
+    packages=find_packages('.'),
+    scripts=['bin/disktool'],
+    namespace_packages=['qingcloud'],
+)
